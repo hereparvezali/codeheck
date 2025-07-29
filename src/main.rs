23ea7f91877e2a0465292361c6450fb2d09c3653
Parvez Ali <urparvezali@gmail.com>
2025-07-30 03:33:35 +0600

    entity generation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-#![allow(unused)]
-
-use std::{env, sync::Arc, time::Duration};
+use std::{env, sync::Arc};
 
 use axum::{Router, routing::get};
 use dotenvy::dotenv;
+use entity::prelude::*;
+use sea_orm::{Database, DatabaseConnection};
 use tokio::{net::TcpListener, sync::Mutex};
+
+pub mod entity;
 
 #[derive(Debug, Clone)]
 pub struct AppState {
-    // pub db: Arc<Mutex<Pool<MySql>>>,
+    pub db: Arc<Mutex<DatabaseConnection>>,
 }
 
 #[tokio::main]
@@ -17,14 +19,16 @@ async fn main() -> Result<(), anyhow::Error> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL")?;
+    let db = Database::connect(database_url).await?;
 
     let app: Router = Router::new()
         .route("/", get(async || "Hellow there!!".to_string()))
         .fallback(async || "Maybe Invalid URL🤔".to_string())
-        .with_state(AppState {});
+        .with_state(AppState {
+            db: Arc::new(Mutex::new(db)),
+        });
 
     let tcp_listener = TcpListener::bind("localhost:8000").await?;
     axum::serve(tcp_listener, app).await?;
-
     Ok(())
 }
